2bd10d1d8ac2535263ec588a649acf44a12b67ce
qieiu <ammuliarizqie@gmail.com>
2024-04-12 09:02:13 +0800

    Initial commit

---
+++ lib/screens/news_screen.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:my_app/dto/News.dart';
+import 'package:my_app/services/data_services.dart';
+
+class NewsScreen extends StatefulWidget {
+  const NewsScreen({Key? key}) : super(key: key);
+
+  @override
+  _NewsScreenState createState() => _NewsScreenState();
+}
+
+class _NewsScreenState extends State<NewsScreen> {
+  Future<List<News>>? _news;
+
+  @override
+  void initState() {
+    super.initState();
+    _news = DataService.fetchNews();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Fetch Data News'),
+      ),
+      body: Center(
+        child: FutureBuilder<List<News>>(
+          future: _news,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return ListView.builder(
+                itemCount: snapshot.data!.length,
+                itemBuilder: (context, index) {
+                  final post = snapshot.data![index];
+                  return ListTile(
+                    title: Text(post.title),
+                    subtitle: Text(post.body),
+                  );
+                },
+              );
+            } else if (snapshot.hasError) {
+              return Text('${snapshot.error}');
+            }
+            //Show a loading indicator while waiting for data
+            return const CircularProgressIndicator();
+          },
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/services/data_services.dart
@@ -0,0 +1,19 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+import 'package:my_app/dto/News.dart';
+import 'package:my_app/endpoints/endpoints.dart';
+
+
+class DataService {
+  static Future<List<News>> fetchNews() async {
+    final response = await http.get(Uri.parse(Endpoints.news));
+    if (response.statusCode == 200) {
+      final List<dynamic> jsonResponse = jsonDecode(response.body);
+      return jsonResponse.map((item) => News.fromJson(item)).toList();
+      } else {
+// handle error
+      throw Exception('Failed to load news');
+      }
+  }
+}
